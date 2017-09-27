eea182576c739dc325e67045c0fb1c62d6def865
Clayton Pabst <claytonpabst@gmail.com>
2017-09-27 15:33:55 -0600

    All sections made, not styled

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,11 +7,19 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="home">
-
-          Home Page
-
-      </div>
+      <section className="home">
+        <header className='headerSection'>Header</header>
+        <div className='section1'>Section 1</div>
+        <div className='section2'>Section 2</div>
+        <div className='whatsNewSection'>What's New</div>
+        <div className='appsAndGamesSection'>Apps and Games</div>
+        <div className='alexaSection'>Alexa Yo!</div>
+        <div className='servicesAndAPIsSection'>Services and APIs</div>
+        <div className='devicesSection'>Devices</div>
+        <div className='socialMediaStrip'>Social Media</div>
+        <div className='footerSection'>I am a footer</div>
+        <div className='allRightsReserved'>All Rights Reserved</div>
+      </section>
     );
   }
 }
