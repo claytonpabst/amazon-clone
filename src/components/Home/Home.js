3fa18be6ec5576ab024e2382e98e4f108a8ea226
Clayton Pabst <claytonpabst@gmail.com>
2017-09-28 16:42:06 -0600

    Header Almost done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,26 @@ class Home extends Component {
   render() {
     return (
       <section className="home">
-        <header className='headerSection'>Header</header>
+        <header className='headerSection'>
+          <div className='headerSectionLogo'>
+            <img src="https://images-na.ssl-images-amazon.com/images/G/01/mobile-apps/dex/amazon_developer._CB288408811_.png"/>
+          </div>
+          <div className='headerSectionNav'>
+            <a>Apps & Games</a>
+            <a>Alexa</a>
+            <a>AWS</a>
+            <a>Services & APIs</a>
+            <a>Devices</a>
+            <a>Blog</a>
+            <a>Docs</a>
+            <a>Support</a>
+          </div>
+          <div className='headerSectionRight'>
+            <a>Sign In</a>
+            <a>English</a>
+            <a>Search</a>
+          </div>
+        </header>
         <div className='section1'>Section 1</div>
         <div className='section2'>Section 2</div>
         <div className='whatsNewSection'>What's New</div>
