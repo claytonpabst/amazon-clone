a8e9bff2a3c0676a8a961d614d46cff9217f13b9
Clayton Pabst <claytonpabst@gmail.com>
2017-10-02 19:07:24 -0600

    header resizing fixed

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -12,25 +12,27 @@ export default function Header() {
 
     return (
         <header className='headerSection'>
-          <div className='headerSectionLogo'>
-            <img src="https://images-na.ssl-images-amazon.com/images/G/01/mobile-apps/dex/amazon_developer._CB288408811_.png"/>
-          </div>
-          <div className='headerSectionNav'>
-            <a>Apps & Games</a>
-            <a>Alexa</a>
-            <a>AWS</a>
-            <a>Services & APIs</a>
-            <a>Devices</a>
-            <a>Blog</a>
-            <a>Docs</a>
-            <a>Support</a>
-          </div>
-          <div className='headerSectionRight'>
-            <a>Sign In</a>
-            <a>English</a>
-            <div style={headerSearchStyle}></div>
-            <a>Search</a>
-          </div>
+          <section className='headerSectionWrapper'>
+            <div className='headerSectionLogo'>
+              <img src="https://images-na.ssl-images-amazon.com/images/G/01/mobile-apps/dex/amazon_developer._CB288408811_.png"/>
+            </div>
+            <div className='headerSectionNav'>
+              <a>Apps & Games</a>
+              <a>Alexa</a>
+              <a>AWS</a>
+              <a>Services & APIs</a>
+              <a>Devices</a>
+              <a>Blog</a>
+              <a>Docs</a>
+              <a>Support</a>
+            </div>
+            <div className='headerSectionRight'>
+              <a>Sign In</a>
+              <a>English</a>
+              <div style={headerSearchStyle}></div>
+              <a>Search</a>
+            </div>
+          </section>
         </header>
     )
 }
