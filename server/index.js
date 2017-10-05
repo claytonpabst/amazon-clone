8b00ef4918f5f56b8d79b737b38fba8b277aa426
Clayton Pabst <claytonpabst@gmail.com>
2017-10-05 14:08:53 -0600

    this guy is set-up for hosting

--- server/index.js
+++ server/index.js
@@ -2,69 +2,69 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 var massive = require('massive');
-var passport = require('passport');
-var session = require('express-session');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// var passport = require('passport');
+// var session = require('express-session');
+// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var config = require('./config.js');
 
 const app = module.exports = express();
 
 app.use(bodyParser.json());
-app.use(session({
-  secret: config.secret,
-    resave: true,
-    saveUninitialized: false,
-    cookie:{
-      maxAge: (1000*60*60*24*14) //this is 14 days
-    }
-}))
+// app.use(session({
+//   secret: config.secret,
+//     resave: true,
+//     saveUninitialized: false,
+//     cookie:{
+//       maxAge: (1000*60*60*24*14) //this is 14 days
+//     }
+// }))
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-massive(config.connection)
-.then( db => {
-  app.set('db', db);
-})
+// massive(config.connection)
+// .then( db => {
+//   app.set('db', db);
+// })
 
 
 app.use(express.static(__dirname + './../build'))
 
-var userController = require("./userController.js");
+// var userController = require("./userController.js");
 
 /////////////Oauth functions
 
-passport.use(new GoogleStrategy({
-  clientID: config.auth0.clientID,
-  clientSecret: config.auth0.clientSecret,
-  callbackURL: '/auth/callback'
-  },
-  userController.findById
+// passport.use(new GoogleStrategy({
+//   clientID: config.auth0.clientID,
+//   clientSecret: config.auth0.clientSecret,
+//   callbackURL: '/auth/callback'
+//   },
+//   userController.findById
 
-));
-passport.serializeUser(function(user,done){
-  console.log('serializing',user);
-  done(null,user);
-});
-passport.deserializeUser(function(id,done){
-  db.find_by_id([id],function(err,user){
-    done(err,user);
-  });
-});
+// ));
+// passport.serializeUser(function(user,done){
+//   console.log('serializing',user);
+//   done(null,user);
+// });
+// passport.deserializeUser(function(id,done){
+//   db.find_by_id([id],function(err,user){
+//     done(err,user);
+//   });
+// });
 
 ///////Oauth endpoints
-app.get('/getuserinfo',userController.getUserInfo);
+// app.get('/getuserinfo',userController.getUserInfo);
 
-app.get('/logout',function(req,res){
-  req.session.destroy(function(err,data){
-  });
-});
+// app.get('/logout',function(req,res){
+//   req.session.destroy(function(err,data){
+//   });
+// });
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/calendar'] }));
+// app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/calendar'] }));
 
-app.get('/auth/callback', passport.authenticate('google', { failureRedirect: '/LoginPage' }), function (req, res) {
-    res.redirect('/');
-});
+// app.get('/auth/callback', passport.authenticate('google', { failureRedirect: '/LoginPage' }), function (req, res) {
+//     res.redirect('/');
+// });
 
 //////////Other endpoints for the front end
 
